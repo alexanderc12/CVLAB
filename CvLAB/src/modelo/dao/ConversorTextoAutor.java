494a1c528d15893ff617c088090c75bc549a2bc7
alexanderc12 <alexanderc@outlook.com>
2015-06-24 13:59:58 -0500

    Agregados varios campos.

--- CvLAB/src/modelo/dao/ConversorTextoAutor.java
+++ CvLAB/src/modelo/dao/ConversorTextoAutor.java
@@ -1,6 +1,7 @@
 package modelo.dao;
 
 import java.io.IOException;
+import java.text.Normalizer;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,19 +25,24 @@ import persistencia.LectorArchivosTextos;
  */
 public class ConversorTextoAutor {
 	
+	private static final String SOLO_ASCII = "[^\\p{ASCII}]";
 	private static final String SEPARADOR = "|";
 	private static final String TAG_REVISTA = "(<br>)(.)+";
 	private static final String TAG_PAIS = "(En: )(.)+";
 	private static final String TAG_TITULO = "\".+\"";
+	private static final String TAG_ANIO = ",\\d{4}";
+	private static final String TAG_ISSN = "(ISSN:</i>)(.)+";
 	private static final String TAG_ARTICULO = "blockquote";
 	private static final String REG_NO_ESPACIOS = "(\\s{3,})";
 	private static final String REG_NO_DOBLE_ESPACIO = "(\\s{2})";
 	private static final String TAG_NOMBRE = "<td>Nombre</td>(.*?)</td>";
-	private static String texto;
-	private static Document documento;
 	private static final String INICIO_CONTENIDO = "<!--Bibliografica-->";
 	private static final String FIN_CONTENIDO = "<td width=\"100%\"><a name=\"libros\"></a>";
 	private static final String REG_NO_HTML = "<[^>]*>";
+	private static String texto;
+	private static Document documento;
+	private static String textoWeb;
+	private static StringBuilder articulo;
 	
 	
 	private static void extraerTextoArticulo(String textoInicio, String textoFin) {
@@ -45,33 +51,90 @@ public class ConversorTextoAutor {
 		documento = Jsoup.parse(texto.substring(indexInicioMetadatos, indexFinMetadatos));
 	}
 	
-	public static void analizarAutores() throws IOException {
-		List<String> lista = LectorArchivosTextos.leerEnlacesAutores();
-		for (String enlaceAutor : lista) {
-			extraerArticulosAutor(enlaceAutor);
+	private static String obtenerDato(String expresion, String texto, int respuesta) {
+		Pattern patron = Pattern.compile(expresion);
+		Matcher interpretador = patron.matcher(texto);
+		String resultado = null;
+		if (interpretador.find()) {
+			resultado = interpretador.group(respuesta);
 		}
+		return resultado;
 	}
 	
+	private static String obtenerDato(String expresion, String texto) {
+		return obtenerDato(expresion, texto, 0);
+	}
+	
+	private static String agregarNombre(StringBuilder articulo) {
+		String nombre = quitarAcentos(normalizarNombre(obtenerDato(TAG_NOMBRE, texto).replace("Nombre", "")
+				.replaceAll(REG_NO_DOBLE_ESPACIO, " ").replaceAll(REG_NO_ESPACIOS, "").replaceAll(REG_NO_HTML, "")));
+		articulo.append(nombre);
+		articulo.append(SEPARADOR);
+		return nombre;
+	}
+	
+	private static void agregarCoautores(String nombre) {
+		String[] listaAutores = textoWeb.substring(0, textoWeb.indexOf("\"") - 2).split(",");
+		int numeroAutores = 0;
+		for (int i = 0; i < 6; i++) {
+			if (i < listaAutores.length) {
+				String coAutor = listaAutores[i];
+				if (coAutor.startsWith(" ")) {
+					coAutor = listaAutores[i].substring(1);
+				}
+				if (!nombre.equalsIgnoreCase(coAutor)) {
+					articulo.append(coAutor);
+					numeroAutores++;
+				}
+			}
+			articulo.append(SEPARADOR);
+		}
+		articulo.append(numeroAutores);
+		articulo.append(SEPARADOR);
+	}
+
+	private static void agregarTitulo() {
+		articulo.append(obtenerDato(TAG_TITULO, textoWeb).toUpperCase());
+		articulo.append(SEPARADOR);
+	}
+	
+	private static void agregraPais() {
+		String pais = obtenerDato(TAG_PAIS, textoWeb).substring(4);
+		articulo.append(pais.substring(0, pais.length() - 1));
+		articulo.append(SEPARADOR);
+	}
+	
+	private static void agregarRevista() {
+		articulo.append(obtenerDato(TAG_REVISTA, textoWeb).substring(5));
+		articulo.append(SEPARADOR);
+	}
+	
+	private static void agregarISSN() {
+		articulo.append(obtenerDato(TAG_ISSN, textoWeb).substring(10));
+		articulo.append(SEPARADOR);
+	}
+	
+	private static void agregarAnio() {
+		articulo.append(obtenerDato(TAG_ANIO, textoWeb).substring(1));
+		articulo.append(System.lineSeparator());
+	}
+
 	public static void extraerArticulosAutor(String enlace) throws IOException {
 		texto = LectorWeb.leerArticulo(enlace);
-		StringBuilder articulo = new StringBuilder();
-		articulo.append(normalizarNombre(obtenerDato(TAG_NOMBRE, texto).replace("Nombre", "")
-				.replaceAll(REG_NO_DOBLE_ESPACIO, " ")
-				.replaceAll(REG_NO_ESPACIOS, "").replaceAll(REG_NO_HTML, "")));
-		articulo.append(SEPARADOR);
+		articulo = new StringBuilder();
+		String nombre = agregarNombre(articulo);
 		extraerTextoArticulo(INICIO_CONTENIDO, FIN_CONTENIDO);
 		Elements listaArticulos = documento.select(TAG_ARTICULO);
 		int indexDatosAutor = articulo.length();
 		for (Element articuloWeb : listaArticulos) {
-			String texto = articuloWeb.html();
 			articulo.delete(indexDatosAutor, articulo.length());
-			articulo.append(obtenerDato(TAG_TITULO, texto).toUpperCase());
-			articulo.append(SEPARADOR);
-			String pais = obtenerDato(TAG_PAIS, texto).substring(4);
-			articulo.append(pais.substring(0, pais.length() - 1));
-			articulo.append(SEPARADOR);
-			articulo.append(obtenerDato(TAG_REVISTA, texto).substring(5));
-			articulo.append(System.lineSeparator());
+			textoWeb = articuloWeb.html();
+			agregarCoautores(nombre);
+			agregarTitulo();
+			agregraPais();
+			agregarRevista();
+			agregarISSN();
+			agregarAnio();
 			LectorArchivosTextos.escribirLineaArchivo(articulo.toString());
 		}
 	}
@@ -87,17 +150,13 @@ public class ConversorTextoAutor {
 		return nombreNormalizado.toString().substring(0, nombreNormalizado.toString().length() - 1);
 	}
 
-	private static String obtenerDato(String expresion, String texto, int respuesta) {
-		Pattern patron = Pattern.compile(expresion);
-		Matcher interpretador = patron.matcher(texto);
-		String resultado = null;
-		if (interpretador.find()) {
-			resultado = interpretador.group(respuesta);
-		}
-		return resultado;
+	private static final String quitarAcentos(String nombre) {
+		return Normalizer.normalize(nombre, Normalizer.Form.NFD).replaceAll(SOLO_ASCII, "");
 	}
-	
-	private static String obtenerDato(String expresion, String texto) {
-		return obtenerDato(expresion, texto, 0);
+	public static void analizarAutores() throws IOException {
+		List<String> lista = LectorArchivosTextos.leerEnlacesAutores();
+		for (String enlaceAutor : lista) {
+			extraerArticulosAutor(enlaceAutor);
+		}
 	}
 }
